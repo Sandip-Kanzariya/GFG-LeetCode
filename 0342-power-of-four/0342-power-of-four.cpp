0ec17aa264efbc17ed2f748c70545a015a194c77
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-10-23 10:12:40 +0530

    Time: 0 ms (100%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
 
+    // bool double_is_int(double d){
+    //     return floor(d) == ceil(d);
+    // }
     bool double_is_int(double d){
-        return floor(d) == ceil(d);
+        return (d - trunc(d) == 0);
     }
-
     bool isPowerOfFour(int n) {
 
         if(n == 0) return false;
