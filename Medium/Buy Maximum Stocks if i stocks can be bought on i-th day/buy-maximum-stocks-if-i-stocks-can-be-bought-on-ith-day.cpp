fdcd68e73df4c06950e7d1f2eb292da7bed268f5
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-12-25 11:05:03 +0530

    Updated Solution - GfG to GitHub

--- Medium/Buy Maximum Stocks if i stocks can be bought on i-th day/buy-maximum-stocks-if-i-stocks-can-be-bought-on-ith-day.cpp
+++ Medium/Buy Maximum Stocks if i stocks can be bought on i-th day/buy-maximum-stocks-if-i-stocks-can-be-bought-on-ith-day.cpp
@@ -10,23 +10,32 @@ public:
     int buyMaximumProducts(int n, int k, int price[]){
         //Write your code here
         
-        vector<pair<int,int>>vp;
-        for (int i=0;i<n;i++){
-            vp.push_back({price[i],i+1});
+        vector<pair<int,int>> vp;
+            
+        for(int i = 0;i < n;i++){
+            
+            vp.push_back({price[i], i + 1});
         }
-        int tot=0;
-        sort(vp.begin(),vp.end());
-        for (auto it:vp){
-            int val=it.first;
-            int num=it.second;
-            int acc=k/val;
-            int curr_price=val*min(num,acc);
-            if (curr_price<=k){
-                 k-=curr_price;
-                 tot+=min(num,acc);
-            }
+        
+        
+        sort(vp.begin(), vp.end());
+        
+        
+        int ans = 0;
+        for(int i = 0;i < n && k > 0;i++){
+            
+            int q  = floor(k / (double) vp[i].first);            
+            
+            q = min(q, vp[i].second);
+            
+            
+            k -= vp[i].first * q;
+            
+            ans += q;
         }
-        return tot;
+        
+        return ans;
+        
     }
 };
 
