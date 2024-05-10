d24fb2375620cf0c8b8a2d77f003cd8784b296a6
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-05-10 13:23:42 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count pair sum/count-pair-sum.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+  public:
+    int countPairs(int arr1[], int arr2[],  int m, int n, int x) 
+    { 
+        int ans = 0;
+        
+        for(int i = 0;i < m;i++){
+            
+            for(int j = 0;j < n;j++){
+                
+                if(arr1[i] + arr2[j] == x) ans++;        
+                
+                if(arr1[i] + arr2[j] > x) break;
+            }
+        }
+        
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m,n;
+	    cin>>m>>n;
+	    int arr1[m],arr2[n];
+	    int x ;
+	    for(int i=0;i<m;i++)
+	        cin>>arr1[i];
+	    for(int j=0;j<n;j++)
+	        cin>>arr2[j];
+	    cin>>x;
+	    Solution ob;
+	    cout <<ob.countPairs(arr1, arr2, m, n, x)<<endl;
+	}
+	return 0;	 
+}
+
+
+// } Driver Code Ends
