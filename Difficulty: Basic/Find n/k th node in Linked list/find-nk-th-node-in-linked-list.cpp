85c2a8d51a1cfdfd84e9221f70b02cdc24f791af
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-07-06 10:13:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find n/k th node in Linked list/find-nk-th-node-in-linked-list.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int length(Node* head){
+    
+    int l = 0;
+    while(head != NULL){
+        l++;
+        head = head -> next;
+    }
+    return l;
+}
+
+int fractional_node(struct Node *head, int k)
+{
+    int l = length(head);
+    
+    int c = ceil(l / (double)k) - 1;
+    
+    Node* temp = head;
+    while(c--){
+        temp = temp -> next;
+    }
+    return temp -> data;    
+}
+
